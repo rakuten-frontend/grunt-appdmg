5e510f192e01d202d231a76a561e4ee0b609369d
Félicien FRANCOIS <felicien@tweakstyle.com>
2015-08-26 14:49:15 +0200

    Use grunt.util.repeat instead of String.repeat

--- tasks/appdmg.js
+++ tasks/appdmg.js
@@ -36,7 +36,7 @@ module.exports = function (grunt) {
       emitter.on('progress', function (info) {
         if (info.type === 'step-begin') {
           var line =  '[' + (info.current <= 9 ? ' ' : '') + info.current + '/' + info.total + '] ' + info.title + '...';
-          grunt.log.write(line + String.repeat(' ', 45 - line.length));
+          grunt.log.write(line + grunt.util.repeat(' ', 45 - line.length));
         }
         if (info.type === 'step-end') {
           var op = ({
