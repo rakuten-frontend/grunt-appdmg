368a7b19566fbf30164892d9e2ea8e1904e44314
Hiroyuki Tanjo <expheno@gmail.com>
2016-04-06 12:03:58 +0900

    Comfortable lines

--- tasks/appdmg.js
+++ tasks/appdmg.js
@@ -26,13 +26,16 @@ module.exports = function (grunt) {
       grunt.file.mkdir(dirname);
 
       // Run appdmg module.
-      // Remove logging and use native appdmg's method in the future release.
       emitter = appdmg({basepath: basepath, specification: options, target: filePair.dest});
+
+      // Logging.
+      // This should be removed when appdmg provides logging method.
       emitter.on('progress', function (info) {
         if (info.type === 'step-begin') {
           var line = '[' + (info.current <= 9 ? ' ' : '') + info.current + '/' + info.total + '] ' + info.title + '...';
           grunt.log.write(line + grunt.util.repeat(45 - line.length, ' '));
         }
+
         if (info.type === 'step-end') {
           var op = ({
             ok: ['green', ' OK '],
@@ -42,10 +45,12 @@ module.exports = function (grunt) {
           grunt.log.write('[' + chalk[op[0]](op[1]) + ']\n');
         }
       });
+
       emitter.on('finish', function () {
-        grunt.log.writeln('\nImage: ' + chalk.cyan(filePair.dest) + ' was created');
+        grunt.log.writeln('\nImage: ' + chalk.cyan(filePair.dest) + ' created');
         done();
       });
+
       emitter.on('error', function (err) {
         grunt.log.error(err.toString());
         done(false);
